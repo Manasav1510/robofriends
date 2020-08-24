9dc7dc63460d73350ea3134c47b3e47bdae71be6
Manasa <manasav.git@gmail.com>
2020-08-24 13:29:42 +0530

    updated changes

---
+++ src/component/CardList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "./Card";
+
+const cardList = ({ robots }) => {
+/*  if(true){
+    throw new Error('NOOOO error is here !');
+  }*/
+  return (
+    <div>
+      {robots.map((user, i) => {
+        return (
+          <Card
+            key={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            username={robots[i].username}
+            email={robots[i].email}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default cardList;
